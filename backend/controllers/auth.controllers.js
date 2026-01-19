545d4d1c7f38ba378f1d63b9825653cebd97b783
01rohitsingh <rohitgidha2000@gmail.com>
2026-01-19 17:44:52 +0530

    signup

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,38 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+import genToken from "../utils/token.js";
+
+// SignUp
+export const signUp = async (req, res) => {
+  try {
+    const { fullName, email, password, mobile, role } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User all ready exist." })
+    }
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 character." })
+    }
+    if (mobile.length < 10) {
+      return res.status(400).json({ message: "Mobile number at least 10 digits." })
+    }
+
+    const hashpassword= await bcrypt.hash(password,10)
+    user=await User.create({
+      fullName,email,role,mobile,password:hashpassword
+    })
+    // Token import
+    const token=await genToken(user._id);
+    // cookie
+    res.cookie("token",token,{
+      secure:false,
+      sameSite:"strict",
+      maxAge: 7*24*60*60*1000,
+      httpOnly:true
+    })
+    return res.status(201).json(user)
+  }
+  catch (error) {
+    return res.status(500).json(`sign up error ${error}`)
+  }
+}
